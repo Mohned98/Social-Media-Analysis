5b58c3b958fbbc66b69debff133ec8d7d3346779
Mostafa Mohamed Ali <Mostafa.M.A.Badr@gmail.com>
2019-05-21 19:51:34 +0200

    Adding the degree centrality function

--- degree_centrality.cpp
+++ degree_centrality.cpp
@@ -2,7 +2,10 @@
 
 void Graph::DegreeCentrality()
 {
-
+// Compute_degree_centrality of adjacency_matrix and store it in output_centrality
+	for(int i=0; i<nodes_n; i++){
+		output_centrality[i] = vect[i].size();
+	}
 
 
 }
