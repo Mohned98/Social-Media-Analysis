57c593788286bae29b39bb67c721a262a4e89f67
Mostafa Mohamed Ali <Mostafa.M.A.Badr@gmail.com>
2019-05-21 19:54:45 +0200

    Adding input and output functions to class Graph

--- Graph.cpp
+++ Graph.cpp
@@ -2,17 +2,42 @@
 
 Graph::Graph()
 {
-	
+
 }
 
 void Graph::Read_InputFile()
 {
+	// Create the object for the input file then open it
+	ifstream inputFile;
 
+	// Open the file (the below address should be modified)
+	inputFile.open("E:\\College\\3rd Computer\\Second term\\Data Structures\\Project Social Media Analysis\\degree_centrality_test\\input.txt");
+
+	// Nodes and weights in file
+	int node1, node2, weight;
+
+	// The first line of the file contains the number of nodes and edges (nodes_n & edges_n)
+	inputFile >> nodes_n >> edges_n;
+
+	// Initialize the adjacency matrix (vector of vectors) and the output vector
+	vect.resize(nodes_n);
+	output_centrality.resize(nodes_n);
+
+	// Fill the adjacency matrix with weights
+	for(int i=0; i<edges_n; i++){
+		inputFile >> node1 >> node2 >> weight;
+		vect[node1].push_back(make_pair(node2, weight));
+		vect[node2].push_back(make_pair(node1, weight));
+	}
+	// Close the opened file
+	inputFile.close();
 }
 
 void Graph::PrintOutput()
 {
-
+	for(int i=0; i<nodes_n; i++){
+		cout << output_centrality[i] << endl;
+	}
 }
 
 Graph::~Graph()
--- Graph.h
+++ Graph.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
 #include <iostream>
-#include <sstream>
+#include <fstream>
 using namespace std;
 class Graph
 {
