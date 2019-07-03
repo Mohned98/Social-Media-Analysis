07f65f55b6175d60a6ce0195ab792d86eccf9091
mostafaezz97 <mostafaezz1997@yahoo.com>
2019-07-04 00:10:27 +0200

    these are the excecutable file that runs the program directly on of these file runs only the closness centrality code and the other runs the degree centrality with their visualizations

--- trial.js
+++ trial.js
@@ -1,9 +1,9 @@
  var graph = {
 	nodes: [
- 	 	{ id: "n0", label: "0", x: 0, y: 0, size: 76.6667, color: '#17496736'},
+ 	 	{ id: "n0", label: "0", x: 0, y: 0, size: 80, color: '#18894836'},
  	 	{ id: "n1", label: "1", x: 0.58, y: 1.1, size: 106, color: '#255'},
- 	 	{ id: "n2", label: "2", x: 1.36, y: -0.08, size: 76.6667, color: '#17496736'},
- 	 	{ id: "n3", label: "3", x: 0.48, y: 1.13, size: 63.3333, color: '#11904336'}
+ 	 	{ id: "n2", label: "2", x: 1.36, y: -0.08, size: 80, color: '#18894836'},
+ 	 	{ id: "n3", label: "3", x: 0.48, y: 1.13, size: 74, color: '#16378256'}
  	],
 	edges: [
  	 	{ id: "e0", source: "n0" , target: "n1", color: '#65280', type:'line', size: 19.5},
